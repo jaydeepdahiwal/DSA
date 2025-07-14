cd4fbacfcb6af281f9588d486bc021aae1bab313
jaydeepdahiwal <71225183+jaydeepdahiwal@users.noreply.github.com>
2025-07-14 14:11:24 +0530

    Time: 3 ms (83.08%) | Memory: 44.9 MB (73.57%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -2,25 +2,26 @@ class Solution {
     public String reverseVowels(String s) {
         char[] sArr = s.toCharArray();
         int left = 0;
-        int right = sArr.length - 1;
+        int right = sArr.length -1;
         while(left < right){
-            if((Character.toLowerCase(sArr[left]) == 'a' || Character.toLowerCase(sArr[left]) == 'e' || Character.toLowerCase(sArr[left]) == 'i' || Character.toLowerCase(sArr[left]) == 'o' || Character.toLowerCase(sArr[left]) == 'u') && ((Character.toLowerCase(sArr[right]) == 'a') || Character.toLowerCase(sArr[right]) == 'e' || Character.toLowerCase(sArr[right]) == 'i' || Character.toLowerCase(sArr[right]) == 'o' || Character.toLowerCase(sArr[right]) == 'u'))
-            {
+            if(isVowel(Character.toLowerCase(sArr[left])) && isVowel(Character.toLowerCase(sArr[right]))){
                 char temp = sArr[left];
                 sArr[left] = sArr[right];
                 sArr[right] = temp;
                 left++;
                 right--;
-            }
-            else{
-                if(!(Character.toLowerCase(sArr[left]) == 'a' || Character.toLowerCase(sArr[left]) == 'e' || Character.toLowerCase(sArr[left]) == 'i' || Character.toLowerCase(sArr[left]) == 'o' || Character.toLowerCase(sArr[left]) == 'u')){
-                    left++;
-                }
-                if(!(Character.toLowerCase(sArr[right]) == 'a' || Character.toLowerCase(sArr[right]) == 'e' || Character.toLowerCase(sArr[right]) == 'i' || Character.toLowerCase(sArr[right]) == 'o' || Character.toLowerCase(sArr[right]) == 'u')){
-                    right--;
-                }
+            }else{
+                if(!isVowel(Character.toLowerCase(sArr[left]))) left++;
+                if(!isVowel(Character.toLowerCase(sArr[right]))) right--;
             }
         }
         return new String(sArr);
+    }
+
+    public boolean isVowel(char c){
+        if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'){
+            return true;
+        }
+        return false;
     }
 }
